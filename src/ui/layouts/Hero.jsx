59ad20e37eae7d34bedbf0ce4a1566fa873934ba
akbarz <barz.develop@gmail.com>
2025-12-17 22:28:35 +0700

    add: base hero section

--- src/ui/layouts/Hero.jsx
+++ src/ui/layouts/Hero.jsx
@@ -1,11 +1,21 @@
 export const HeroSection = () => {
   return (
-    <div className="h-dvh flex items-center justify-center">
-      <div>
-        <h1>Title App</h1>
-        <div>
-          <button>Button</button>
-        </div>
+    <div className="h-dvh flex items-center justify-center flex-col">
+      <div className="text-white ">
+        <h1 className="text-7xl font-bold">Next Step: Karierku</h1>
+        <h2 className="text-3xl pt-2">
+          Sistem Rekomendasi Jalur Karier Berbasis Keahlian
+        </h2>
+      </div>
+      <div className="pt-2">
+        <button
+          onClick={() => {
+            alert("hello");
+          }}
+          className="border px-4 py-2 rounded-lg text-white  my-2 cursor-pointer"
+        >
+          Coba Analisis
+        </button>
       </div>
     </div>
   );
